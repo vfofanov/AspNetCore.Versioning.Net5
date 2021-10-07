78765bfeae35451785b0f5ed434edc1d0b9d7590
Vasiliy Fofanov <vfofanov@gmail.com>
2021-10-07 03:27:13 +0300

    Implement custom api versioning

---
+++ src/OData8VersioningPrototype/ApiConventions/VersioningRoutingApplicationModelProvider.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.OData.Extensions;
+using Microsoft.AspNetCore.OData.Routing.Attributes;
+using Microsoft.AspNetCore.OData.Routing.Template;
+using Microsoft.OData.Edm;
+
+namespace OData8VersioningPrototype.ApiConventions
+{
+    public class VersioningRoutingApplicationModelProvider : IApplicationModelProvider
+    {
+        //After all
+        public int Order => 2000;
+        
+        public void OnProvidersExecuted(ApplicationModelProviderContext context)
+        {
+            foreach (var controller in context.Result.Controllers)
+            {
+                if (controller.Attributes.OfType<ODataAttributeRoutingAttribute>().Any())
+                {
+                    continue;
+                }
+
+                foreach (var action in controller.Actions)
+                {
+                    
+                }
+                
+            }
+        }
+
+        public void OnProvidersExecuting(ApplicationModelProviderContext context)
+        {
+        }
+    }
+}
