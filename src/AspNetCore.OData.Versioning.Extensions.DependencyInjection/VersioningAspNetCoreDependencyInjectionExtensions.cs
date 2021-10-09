8228482fef2c91b0413c3ea85d7334db6d6ecb49
Vasiliy Fofanov <vfofanov@gmail.com>
2021-10-10 00:27:30 +0300

    Cleanup packages. Add Dependency injection extensions projects

---
+++ src/AspNetCore.OData.Versioning.Extensions.DependencyInjection/VersioningAspNetCoreDependencyInjectionExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCore.OData.Versioning.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Dependency injection extensions for registee AspNet.Core OData versioning 
+    /// </summary>
+    public static class VersioningAspNetCoreODataDependencyInjectionExtensions
+    {
+    }
+}
---
+++ src/AspNetCore.Versioning.Extensions.DependencyInjection/VersioningAspNetCoreDependencyInjectionExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCore.Versioning.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Dependency injection extensions for register AspNet.Core versioning 
+    /// </summary>
+    public static class VersioningAspNetCoreDependencyInjectionExtensions
+    {
+    }
+}
