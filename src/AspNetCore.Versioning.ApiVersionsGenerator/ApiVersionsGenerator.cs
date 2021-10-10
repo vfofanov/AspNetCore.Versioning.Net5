f2dbd951adf556ccbe491392c14100a10d817b55
Vasiliy Fofanov <vfofanov@gmail.com>
2021-10-11 00:07:59 +0300

    Initial commit

---
+++ src/AspNetCore.Versioning.ApiVersionsGenerator/ApiVersionsGenerator.cs
@@ -0,0 +1,34 @@
+﻿//Uncomment for debug
+//#define DEBUG_GENERATION
+
+using System.Diagnostics;
+using Microsoft.CodeAnalysis;
+
+namespace AspNetCore.Versioning.ApiVersionsGenerator
+{
+        [Generator]
+        public class ApiVersionsGenerator : ISourceGenerator
+        {
+#if DEBUG && DEBUG_GENERATION
+        private const bool launchDebugger = true;
+#else
+            private const bool launchDebugger = false;
+#endif
+
+            public virtual void Initialize(GeneratorInitializationContext context)
+            {
+                if (launchDebugger)
+                {
+                    if (!Debugger.IsAttached)
+                    {
+                        Debugger.Launch();
+                    }    
+                }
+            }
+
+            /// <inheritdoc />
+            public void Execute(GeneratorExecutionContext context)
+            {
+            }
+        }
+}
---
+++ src/AspNetCore.Versioning.ApiVersionsGenerator/SourceGeneratorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace AspNetCore.Versioning.ApiVersionsGenerator
+{
+    public static class SourceGeneratorExtensions
+    {
+        public static bool TryReadGlobalBuildProperty(this GeneratorExecutionContext context, string property, out string value)
+        {
+            return context.AnalyzerConfigOptions.GlobalOptions.TryGetValue($"build_property.{property}", out value);
+        }
+
+        public static bool TryReadAdditionalFilesAttribute(this GeneratorExecutionContext context, AdditionalText additionalText, string property, out string value)
+        {
+            return context.AnalyzerConfigOptions.GetOptions(additionalText).TryGetValue($"build_metadata.AdditionalFiles.{property}", out value);
+        }
+    }
+}
